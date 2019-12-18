789471b17fd688d74a99c5e5e651227618f631a4
zuprocj <jmdcorpuz@gmail.com>
2019-12-18 14:05:37 -0800

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# Game of Clicks
-
-Test you memory of Game of Thrones characters with the Game of Clicks game.  Game of Clicks is a memory skill application built using React.  The objective of the game is to be able to go through and clock each of the charcter image once. Each time you click a character, your score goes up by one. if you clock a character;s image twice, you lose and the game resets.  Your score is goes back to 0 and the current top score is displayed.
-
---
+++ precache-manifest.d3a0f383704bf7efcef84b80e21ae701.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdab15cf0cb8a5651007c728ba1c8b92",
+    "url": "/gameofclicks/index.html"
+  },
+  {
+    "revision": "e69eecae7ec7d1f71420",
+    "url": "/gameofclicks/static/css/main.0c9d63ce.chunk.css"
+  },
+  {
+    "revision": "4e6dc0637d1113a61819",
+    "url": "/gameofclicks/static/js/2.e7f25eeb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/gameofclicks/static/js/2.e7f25eeb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e69eecae7ec7d1f71420",
+    "url": "/gameofclicks/static/js/main.7e3c6283.chunk.js"
+  },
+  {
+    "revision": "94508489e36a9b6b6d38",
+    "url": "/gameofclicks/static/js/runtime-main.1fdfc55f.js"
+  }
+]);
